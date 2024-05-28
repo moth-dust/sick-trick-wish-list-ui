bea3b932ac217766ddb2855d21d868cb4a621b86
Gwyneth Patrick <gwynethpatrick235@gmail.com>
2024-05-28 09:33:41 -0600

    feat: Will create a pesudocoded architecture.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import React from 'react';
 
-
+// component imports
+// api calls
+// tricks state
 
 function App() {
   return (
     <div className="App">
       <h1>Sick Trick Wish List</h1>
+      {/* form component */}
+      {/* tricks component */}
     </div>
   );
 }
