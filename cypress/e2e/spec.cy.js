ef64b7ed1c2668d880c6f75ae07925d2f9ce6d3c
Gwyneth Patrick <gwynethpatrick235@gmail.com>
2024-05-28 11:30:07 -0600

    feat: Will add a set of render tests of loading page.

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,12 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Display Tests', () => {
+  it('Should be able to get all of these elements to assure they exist on page load', () => {
+    cy.visit('http://localhost:3000/')
+    .get('h1')
+    .get('#stance')
+    .get('#trick')
+    .get('#obstacle')
+    .get('#tutorial')
+    .get('button')
+    .get('.trick-card')
   })
 })
