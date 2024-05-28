f40aa415d667f546ec7ceca0c6245b9cafb5c5d0
Gwyneth Patrick <gwynethpatrick235@gmail.com>
2024-05-28 10:39:25 -0600

    feat: Will add form component and link to App.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,10 +2,7 @@ import './App.css';
 import React, { useEffect, useState } from 'react';
 import Tricks from '../Tricks/Tricks';
 import { callTricks } from '../../apiCalls/apiCalls';
-
-// component imports
-// api calls
-// tricks state
+import Form from '../Form/Form';
 
 function App() {
   const [tricks, setTricks] = useState([])
@@ -23,7 +20,7 @@ function App() {
   return (
     <div className="App">
       <h1>Sick Trick Wish List</h1>
-      {/* form component */}
+      <Form/>
       <Tricks tricks = {tricks}/>
     </div>
   );
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,8 @@
+function Form({/**updateFunction */}){
+    return(
+    <div className="trick-submission">
+        <h1>hello world</h1>
+    </div>
+    )
+};
+export default Form;
