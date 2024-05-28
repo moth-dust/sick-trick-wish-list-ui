3d14a65d4e44c687706785a470c1c52adb0462b1
Gwyneth Patrick <gwynethpatrick235@gmail.com>
2024-05-28 10:52:35 -0600

    feat: Will create skeleton of Form.

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,7 +1,18 @@
 function Form({/**updateFunction */}){
+
     return(
     <div className="trick-submission">
-        <h1>hello world</h1>
+        <select className="stance-select">
+            <option value={null}>Choose your Stance</option>
+        </select>
+        <input className="trick-name" placeholder="Name of Trick"></input>
+        <select className="obstacle-select">
+            <option value={null}>Choose your Obstacle</option>
+        </select>
+        <input className="tutorial-link" placeholder="Link to Tutorial"></input>
+        <button onClick={(e)=>{
+            console.log(e.target)
+        }}>Send It!</button>
     </div>
     )
 };
